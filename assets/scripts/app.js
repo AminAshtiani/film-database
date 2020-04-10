a6410cdc952f0937255cc28586f1d4cd6ff2e917
amin ashtiani <amin.ashtiani@snapp.market>
2020-04-10 13:14:54 +0430

    initial commit
    just select modal and button

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+// const addMovieModal = document.body.children[1];
+// const addMovieModal = document.querySelector('#add-modal');
+const addMovieModal = document.getElementById('add-modal');
+
+// const startAddMovieButton = docuemnt.querySelector('header').lastChildElement;
+const startAddMovieButton = document.querySelector('header button');
