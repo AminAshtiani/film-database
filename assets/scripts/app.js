de30b03be28c06176bd00c40c3d27e4510bc9a51
amin ashtiani <amin.ashtiani@snapp.market>
2020-04-10 13:30:29 +0430

    open and dismiss addMovieModal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,22 @@ const addMovieModal = document.getElementById('add-modal');
 
 // const startAddMovieButton = docuemnt.querySelector('header').lastChildElement;
 const startAddMovieButton = document.querySelector('header button');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const backdrop = document.getElementById('backdrop');
+
+const toggleBackdrop = function() {
+    backdrop.classList.toggle('visible');
+}
+const toggleMovieModal = function() {
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+}
+const cancelAddMovie = function() {
+    toggleMovieModal();
+}
+const backdropClickHandler = function() {
+    toggleMovieModal();
+}
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+backdropk.addEventListener('click', backdropClickHandler);
